fa0ea6ce266c88d84229b6a99f364e030f5a2429
jullk <attrG@users.noreply.github.com>
2017-01-09 14:34:32 +0200

    commit before fixes (with socials, etc.)

--- js/common.js
+++ js/common.js
@@ -5,19 +5,27 @@ $(document).ready(function () {
         starSize: 18,
         useGradient: false,
         hoverColor: '#ff7f30'
-        // readOnly: true
     });
+
+    $('.hide-description__btn').first().toggleClass('closed-v--blue');
+    $('.hide-description__btn').first().toggleClass('opened-v--blue');
+    $('.hide-description__btn').first().text('Hide details');
+
 
     $('.js-open-sibling').on('click', function () {
         var current = $(this);
         current.siblings('p').slideToggle();
         current.toggleClass('opened');
         current.toggleClass('closed');
-
     });
 
     $('.js-hide-description').on('click', function () {
         var current = $(this);
+        if (current.text() == "Show details") {
+            current.text("Hide details")
+        } else {
+            current.text("Show details")
+        }
         current.siblings('.score-list').slideToggle();
         current.toggleClass('opened-v--blue');
         current.toggleClass('closed-v--blue');
